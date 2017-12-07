b9388b6afdaa74c12c1a3d23d3835fae375d0914
Constantin Wang <850901938@qq.com>
2017-12-07 23:49:26 +0800

    add #70

---
+++ 70_Climbing Stairs.js
@@ -0,0 +1,38 @@
+// 70. Climbing Stairs
+// DescriptionHintsSubmissionsDiscussSolution
+// Discuss Pick One
+// You are climbing a stair case. It takes n steps to reach to the top.
+
+// Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+
+// Note: Given n will be a positive integer.
+
+
+// Example 1:
+
+// Input: 2
+// Output:  2
+// Explanation:  There are two ways to climb to the top.
+
+// 1. 1 step + 1 step
+// 2. 2 steps
+// Example 2:
+
+// Input: 3
+// Output:  3
+// Explanation:  There are three ways to climb to the top.
+
+// 1. 1 step + 1 step + 1 step
+// 2. 1 step + 2 steps
+// 3. 2 steps + 1 step
+let climbStairs = function(n) {
+    let arr = []
+    arr.push(1)
+    arr.push(2)
+    for(let i=2;i<100;i++){
+        arr.push(arr[i-1]+arr[i-2])
+    }
+    return arr[n-1]
+};
+
+console.log(climbStairs(5))
